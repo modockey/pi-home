98d7e9420c6c896737075ad88be13eb1fce1f9e5
Akira Nakagawa <akira.nakagawa.kmdk@gmail.com>
2022-06-21 00:35:59 +0900

    ip_register開発終了

--- server/ip_register/src/conn.rs
+++ server/ip_register/src/conn.rs
@@ -4,7 +4,9 @@ use dotenv::dotenv;
 use std::env;
 
 pub fn establish_connection() -> PgConnection {
-  dotenv().ok();
+  if cfg!(test) | cfg!(debug_assertions) {
+    dotenv().ok();
+  }
 
   let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
   PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
