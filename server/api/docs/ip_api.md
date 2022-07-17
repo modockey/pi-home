d1d388ddbc96a0a64c23ac4f3e33ef9deed8bf91
Akira Nakagawa <akira.nakagawa.kmdk@gmail.com>
2022-07-17 10:21:39 +0900

    API Server初期コード生成

---
+++ server/api/docs/ip_api.md
@@ -0,0 +1,33 @@
+# ip_api
+
+All URIs are relative to *http://localhost:50001/v1*
+
+Method | HTTP request | Description
+------------- | ------------- | -------------
+**getIP**](ip_api.md#getIP) | **GET** /ip | Get Global IPv4 address of the system
+
+
+# **getIP**
+> models::GetIp200Response getIP()
+Get Global IPv4 address of the system
+
+Get Global IPv4 address of the system
+
+### Required Parameters
+This endpoint does not need any parameter.
+
+### Return type
+
+[**models::GetIp200Response**](getIP_200_response.md)
+
+### Authorization
+
+No authorization required
+
+### HTTP request headers
+
+ - **Content-Type**: Not defined
+ - **Accept**: application/json
+
+[[Back to top]](#) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to Model list]](../README.md#documentation-for-models) [[Back to README]](../README.md)
+
---
+++ server/api/src/models.rs
@@ -0,0 +1,130 @@
+#![allow(unused_qualifications)]
+
+use crate::models;
+#[cfg(any(feature = "client", feature = "server"))]
+use crate::header;
+
+#[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+#[cfg_attr(feature = "conversion", derive(frunk::LabelledGeneric))]
+pub struct GetIp200Response {
+    #[serde(rename = "IPv4_address")]
+    #[serde(skip_serializing_if="Option::is_none")]
+    pub ipv4_address: Option<String>,
+
+    #[serde(rename = "checked_at")]
+    #[serde(skip_serializing_if="Option::is_none")]
+    pub checked_at: Option<chrono::DateTime::<chrono::Utc>>,
+
+}
+
+impl GetIp200Response {
+    pub fn new() -> GetIp200Response {
+        GetIp200Response {
+            ipv4_address: None,
+            checked_at: None,
+        }
+    }
+}
+
+/// Converts the GetIp200Response value to the Query Parameters representation (style=form, explode=false)
+/// specified in https://swagger.io/docs/specification/serialization/
+/// Should be implemented in a serde serializer
+impl std::string::ToString for GetIp200Response {
+    fn to_string(&self) -> String {
+        let mut params: Vec<String> = vec![];
+
+        if let Some(ref ipv4_address) = self.ipv4_address {
+            params.push("IPv4_address".to_string());
+            params.push(ipv4_address.to_string());
+        }
+
+        // Skipping checked_at in query parameter serialization
+
+        params.join(",").to_string()
+    }
+}
+
+/// Converts Query Parameters representation (style=form, explode=false) to a GetIp200Response value
+/// as specified in https://swagger.io/docs/specification/serialization/
+/// Should be implemented in a serde deserializer
+impl std::str::FromStr for GetIp200Response {
+    type Err = String;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        #[derive(Default)]
+        // An intermediate representation of the struct to use for parsing.
+        struct IntermediateRep {
+            pub ipv4_address: Vec<String>,
+            pub checked_at: Vec<chrono::DateTime::<chrono::Utc>>,
+        }
+
+        let mut intermediate_rep = IntermediateRep::default();
+
+        // Parse into intermediate representation
+        let mut string_iter = s.split(',').into_iter();
+        let mut key_result = string_iter.next();
+
+        while key_result.is_some() {
+            let val = match string_iter.next() {
+                Some(x) => x,
+                None => return std::result::Result::Err("Missing value while parsing GetIp200Response".to_string())
+            };
+
+            if let Some(key) = key_result {
+                match key {
+                    "IPv4_address" => intermediate_rep.ipv4_address.push(<String as std::str::FromStr>::from_str(val).map_err(|x| format!("{}", x))?),
+                    "checked_at" => intermediate_rep.checked_at.push(<chrono::DateTime::<chrono::Utc> as std::str::FromStr>::from_str(val).map_err(|x| format!("{}", x))?),
+                    _ => return std::result::Result::Err("Unexpected key while parsing GetIp200Response".to_string())
+                }
+            }
+
+            // Get the next key
+            key_result = string_iter.next();
+        }
+
+        // Use the intermediate representation to return the struct
+        std::result::Result::Ok(GetIp200Response {
+            ipv4_address: intermediate_rep.ipv4_address.into_iter().next(),
+            checked_at: intermediate_rep.checked_at.into_iter().next(),
+        })
+    }
+}
+
+// Methods for converting between header::IntoHeaderValue<GetIp200Response> and hyper::header::HeaderValue
+
+#[cfg(any(feature = "client", feature = "server"))]
+impl std::convert::TryFrom<header::IntoHeaderValue<GetIp200Response>> for hyper::header::HeaderValue {
+    type Error = String;
+
+    fn try_from(hdr_value: header::IntoHeaderValue<GetIp200Response>) -> std::result::Result<Self, Self::Error> {
+        let hdr_value = hdr_value.to_string();
+        match hyper::header::HeaderValue::from_str(&hdr_value) {
+             std::result::Result::Ok(value) => std::result::Result::Ok(value),
+             std::result::Result::Err(e) => std::result::Result::Err(
+                 format!("Invalid header value for GetIp200Response - value: {} is invalid {}",
+                     hdr_value, e))
+        }
+    }
+}
+
+#[cfg(any(feature = "client", feature = "server"))]
+impl std::convert::TryFrom<hyper::header::HeaderValue> for header::IntoHeaderValue<GetIp200Response> {
+    type Error = String;
+
+    fn try_from(hdr_value: hyper::header::HeaderValue) -> std::result::Result<Self, Self::Error> {
+        match hdr_value.to_str() {
+             std::result::Result::Ok(value) => {
+                    match <GetIp200Response as std::str::FromStr>::from_str(value) {
+                        std::result::Result::Ok(value) => std::result::Result::Ok(header::IntoHeaderValue(value)),
+                        std::result::Result::Err(err) => std::result::Result::Err(
+                            format!("Unable to convert header value '{}' into GetIp200Response - {}",
+                                value, err))
+                    }
+             },
+             std::result::Result::Err(e) => std::result::Result::Err(
+                 format!("Unable to convert header: {:?} to string: {}",
+                     hdr_value, e))
+        }
+    }
+}
+
