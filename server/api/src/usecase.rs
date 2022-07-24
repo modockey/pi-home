5f506ea6dec57b3363ea8e008b45001756abc6fa
Akira Nakagawa <akira.nakagawa.kmdk@gmail.com>
2022-07-24 13:08:58 +0900

    テスト修正

--- server/api/src/usecase.rs
+++ server/api/src/usecase.rs
@@ -15,62 +15,66 @@ pub fn get_effective_ipv4_record() -> Result<db::Ipv4Record, String> {
   return Ok(effective_records[0].clone());
 }
 
-use chrono::{offset::TimeZone, Utc};
-use diesel::pg::PgConnection;
-use std::path::Path;
+#[cfg(test)]
+mod tests {
+  use crate::db;
+  use chrono::{offset::TimeZone, Utc};
+  use diesel::pg::PgConnection;
+  use std::path::Path;
 
-#[test]
-fn test_get_ip() {
-  struct Testcase {
-    name: String,
-    sql_file_path: String,
-    expect: Result<db::Ipv4Record, String>,
-  }
+  #[test]
+  fn test_get_ip() {
+    struct Testcase {
+      name: String,
+      sql_file_path: String,
+      expect: Result<db::Ipv4Record, String>,
+    }
 
-  let testcases = vec![
-    Testcase {
-      name: "case_001".to_string(),
-      sql_file_path: "./src/testcase/get_ip/case_001.sql".to_string(),
-      expect: Ok(db::Ipv4Record {
-        id: 2,
-        ipv4_address: "112.112.112.112".to_string(),
-        effective_flg: true,
-        last_checked_at: match Utc.datetime_from_str("2022/01/02 00:00:00", "%Y/%m/%d %H:%M:%S") {
-          Err(why) => panic!("datetime parse error:{}", why),
-          Ok(datetime) => datetime,
-        },
-      }),
-    },
-    Testcase {
-      name: "case_002".to_string(),
-      sql_file_path: "./src/testcase/get_ip/case_002.sql".to_string(),
-      expect: Err("IPv4 record not found".to_string()),
-    },
-    Testcase {
-      name: "case_002".to_string(),
-      sql_file_path: "./src/testcase/get_ip/case_002.sql".to_string(),
-      expect: Err("IPv4 record not found".to_string()),
-    },
-    Testcase {
-      name: "case_003".to_string(),
-      sql_file_path: "./src/testcase/get_ip/case_003.sql".to_string(),
-      expect: Err("Too many IPv4 records have been found".to_string()),
-    },
-  ];
+    let testcases = vec![
+      Testcase {
+        name: "case_001".to_string(),
+        sql_file_path: "./src/testcase/get_ip/case_001.sql".to_string(),
+        expect: Ok(db::Ipv4Record {
+          id: 2,
+          ipv4_address: "112.112.112.112".to_string(),
+          effective_flg: true,
+          last_checked_at: match Utc.datetime_from_str("2022/01/02 00:00:00", "%Y/%m/%d %H:%M:%S") {
+            Err(why) => panic!("datetime parse error:{}", why),
+            Ok(datetime) => datetime,
+          },
+        }),
+      },
+      Testcase {
+        name: "case_002".to_string(),
+        sql_file_path: "./src/testcase/get_ip/case_002.sql".to_string(),
+        expect: Err("IPv4 record not found".to_string()),
+      },
+      Testcase {
+        name: "case_002".to_string(),
+        sql_file_path: "./src/testcase/get_ip/case_002.sql".to_string(),
+        expect: Err("IPv4 record not found".to_string()),
+      },
+      Testcase {
+        name: "case_003".to_string(),
+        sql_file_path: "./src/testcase/get_ip/case_003.sql".to_string(),
+        expect: Err("Too many IPv4 records have been found".to_string()),
+      },
+    ];
 
-  let conn = db::establish_connection();
-  fn reset_db(conn: &PgConnection) {
-    let reset = Path::new("./src/testcase/get_ip/reset.sql");
-    db::exec_sql(reset, &conn).expect("unexpected error on reset");
-  }
+    let conn = db::establish_connection();
+    fn reset_db(conn: &PgConnection) {
+      let reset = Path::new("./src/testcase/get_ip/reset.sql");
+      db::exec_sql(reset, &conn).expect("unexpected error on reset");
+    }
 
-  for testcase in testcases {
-    reset_db(&conn);
+    for testcase in testcases {
+      reset_db(&conn);
 
-    let path = Path::new(&testcase.sql_file_path);
-    db::exec_sql(path, &conn).expect(&format!("error on {}", testcase.name));
+      let path = Path::new(&testcase.sql_file_path);
+      db::exec_sql(path, &conn).expect(&format!("error on {}", testcase.name));
 
-    let result = get_effective_ipv4_record();
-    assert_eq!(result, testcase.expect);
+      let result = super::get_effective_ipv4_record();
+      assert_eq!(result, testcase.expect);
+    }
   }
 }
